921afedc033b6b7dbb63c70ecc8ec2ba1217d2e6
Alexandros Pournaras <apournaras@york.citycollege.eu>
2025-02-05 17:17:00 +0200

    Add text to speech button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +9,15 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-buttons';
+
+  @ViewChild('helloText', { static: true }) helloText!: ElementRef;
+  @ViewChild('congratsText', { static: true }) congratsText!: ElementRef;
+
+  textToSpeech() {
+    const hello = this.helloText.nativeElement.textContent;
+    const congrats = this.congratsText.nativeElement.textContent;
+    const speech = new SpeechSynthesisUtterance(`${hello}. ${congrats}`);
+    window.speechSynthesis.speak(speech);
+  }
 }
+
